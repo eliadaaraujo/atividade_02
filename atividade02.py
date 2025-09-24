c3b3201838100b138179a5ae91c52ffb559aa0b4
eli <eliadadearaujolinhares@gmail.com>
2025-09-24 19:25:48 -0300

    Add files via upload

---
+++ atividade02.py
@@ -0,0 +1,9 @@
+# Atividade 2 – Variáveis Simples
+# Crie variáveis para nome, idade e cidade.
+# Mostre na tela a frase:
+# Meu nome é ___, tenho ___ anos e moro em ___.
+
+nome = "Eliada"
+idade = 16
+cidade = "Tianguá"
+print(f'Meu nome é {nome}, tenho {idade} anos e moro em {cidade}.')
